LineageOS/android_packages_apps_Settings

Settings: Remove dependency on pref object for min refresh rate isAvailable()
Preference is not available during search indexing.

Change-Id: Ia53aee2a0e4d3c6ef8a56d202af3da48cf6b1d0e

src/com/android/settings/display/MinRefreshRatePreferenceController.java
@@ -40,15 +40,29 @@ public class MinRefreshRatePreferenceController extends BasePreferenceController
 
     private ListPreference mListPreference;
 
+    private List<String> mEntries = new ArrayList<>();
+    private List<String> mValues = new ArrayList<>();
+
     public MinRefreshRatePreferenceController(Context context) {
         super(context, KEY_MIN_REFRESH_RATE);
+
+        if (mContext.getResources().getBoolean(R.bool.config_show_min_refresh_rate_switch)) {
+            Display.Mode mode = mContext.getDisplay().getMode();
+            Display.Mode[] modes = mContext.getDisplay().getSupportedModes();
+            for (Display.Mode m : modes) {
+                if (m.getPhysicalWidth() == mode.getPhysicalWidth() &&
+                        m.getPhysicalHeight() == mode.getPhysicalHeight()) {
+                    mEntries.add(String.format("%.02fHz", m.getRefreshRate())
+                            .replaceAll("[\\.,]00", ""));
+                    mValues.add(String.format(Locale.US, "%.02f", m.getRefreshRate()));
+                }
+            }
+        }
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getResources().getBoolean(R.bool.config_show_min_refresh_rate_switch) &&
-                mListPreference != null && mListPreference.getEntries().length > 1
-                        ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return mEntries.size() > 1 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -59,20 +73,8 @@ public class MinRefreshRatePreferenceController extends BasePreferenceController
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mListPreference = screen.findPreference(getPreferenceKey());
-
-        List<String> entries = new ArrayList<>(), values = new ArrayList<>();
-        Display.Mode mode = mContext.getDisplay().getMode();
-        Display.Mode[] modes = mContext.getDisplay().getSupportedModes();
-        for (Display.Mode m : modes) {
-            if (m.getPhysicalWidth() == mode.getPhysicalWidth() &&
-                    m.getPhysicalHeight() == mode.getPhysicalHeight()) {
-                entries.add(String.format("%.02fHz", m.getRefreshRate())
-                        .replaceAll("[\\.,]00", ""));
-                values.add(String.format(Locale.US, "%.02f", m.getRefreshRate()));
-            }
-        }
-        mListPreference.setEntries(entries.toArray(new String[entries.size()]));
-        mListPreference.setEntryValues(values.toArray(new String[values.size()]));
+        mListPreference.setEntries(mEntries.toArray(new String[mEntries.size()]));
+        mListPreference.setEntryValues(mValues.toArray(new String[mValues.size()]));
 
         super.displayPreference(screen);
     }

NurKeinNeid/packages_apps_Settings

src/com/android/settings/display/MinRefreshRatePreferenceController.java
@@ -40,15 +40,29 @@ public class MinRefreshRatePreferenceController extends BasePreferenceController
 
     private ListPreference mListPreference;
 
+    private List<String> mEntries = new ArrayList<>();
+    private List<String> mValues = new ArrayList<>();
+
     public MinRefreshRatePreferenceController(Context context) {
         super(context, KEY_MIN_REFRESH_RATE);
+
+        if (mContext.getResources().getBoolean(R.bool.config_show_refresh_rate_controls)) {
+            Display.Mode mode = mContext.getDisplay().getMode();
+            Display.Mode[] modes = mContext.getDisplay().getSupportedModes();
+            for (Display.Mode m : modes) {
+                if (m.getPhysicalWidth() == mode.getPhysicalWidth() &&
+                        m.getPhysicalHeight() == mode.getPhysicalHeight()) {
+                    mEntries.add(String.format("%.02fHz", m.getRefreshRate())
+                            .replaceAll("[\\.,]00", ""));
+                    mValues.add(String.format(Locale.US, "%.02f", m.getRefreshRate()));
+                }
+            }
+        }
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getResources().getBoolean(R.bool.config_show_refresh_rate_controls) &&
-                mListPreference != null && mListPreference.getEntries().length > 1
-                        ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return mEntries.size() > 1 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -59,20 +73,8 @@ public class MinRefreshRatePreferenceController extends BasePreferenceController
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mListPreference = screen.findPreference(getPreferenceKey());
-
-        List<String> entries = new ArrayList<>(), values = new ArrayList<>();
-        Display.Mode mode = mContext.getDisplay().getMode();
-        Display.Mode[] modes = mContext.getDisplay().getSupportedModes();
-        for (Display.Mode m : modes) {
-            if (m.getPhysicalWidth() == mode.getPhysicalWidth() &&
-                    m.getPhysicalHeight() == mode.getPhysicalHeight()) {
-                entries.add(String.format("%.02fHz", m.getRefreshRate())
-                        .replaceAll("[\\.,]00", ""));
-                values.add(String.format(Locale.US, "%.02f", m.getRefreshRate()));
-            }
-        }
-        mListPreference.setEntries(entries.toArray(new String[entries.size()]));
-        mListPreference.setEntryValues(values.toArray(new String[values.size()]));
+        mListPreference.setEntries(mEntries.toArray(new String[mEntries.size()]));
+        mListPreference.setEntryValues(mValues.toArray(new String[mValues.size()]));
 
         super.displayPreference(screen);
     }