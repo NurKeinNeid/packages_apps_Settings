adi8900/packages_apps_Settings-1

Settings: Checkin of DerpQuest
Based on implementations by DU and OmniROM,
so credits to mazda and maxwen for this.

spezi77:
-Added our new Pixel Dust icon for Ten. Credits to Roger Truttmann!
-Moved PDSettings into System Settings

NurKeinNeid:
-Change icon to Derp owl
-Move to top level settings

idoybh: Adapt to Android.bp

Change-Id: If29de8999bd198afb8190363a42c96a5927879f9
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -46,6 +46,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.derpquest.settings.DerpQuestSettings;
+
 import java.util.Map;
 
 /**
@@ -119,6 +121,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
                 CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(DerpQuestSettings.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 

NurKeinNeid/packages_apps_Settings

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -46,6 +46,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.derpquest.settings.DerpQuestSettings;
+
 import java.util.Map;
 
 /**
@@ -119,6 +121,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
                 CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(DerpQuestSettings.class.getName(),
+                CategoryKey.CATEGORY_QUEST);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 